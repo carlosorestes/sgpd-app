1f7e91737733c02d9932cb4ebddaccd80f858b82
carlosmalaquias <carlosomalaquias@gmail.com>
2020-01-25 07:34:44 -0200

    Add Client Component

--- src/app/client/client/client.component.ts
+++ src/app/client/client/client.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Client } from '../client';
-import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
-import { HttpClientService } from 'src/app/service/http-client.service';
-import { Router, ActivatedRoute } from '@angular/router';
-import { EventEmitterService } from 'src/app/service/event-emitter.service';
-
 
 @Component({
   selector: 'app-client',
@@ -13,22 +7,9 @@ import { EventEmitterService } from 'src/app/service/event-emitter.service';
 })
 export class ClientComponent implements OnInit {
 
-  
-  client: Client = new Client("","","","","");
-
-  constructor(private httpClientService: HttpClientService,
-    private router:Router,
-    private activatedRoute: ActivatedRoute,
-    private eventEmitterService: EventEmitterService) { 
-    }
+  constructor() { }
 
   ngOnInit() {
-    this.activatedRoute.queryParams.subscribe(params => {
-        this.client['id'] = params['id']
-        this.client['nome'] = params['nome'];
-        this.client['cpf'] = params['cpf'];
-        this.client['telefone1'] = params['telefone1'];
-        this.client['telefone2'] = params['telefone2'];
-      });
   }
+
 }
